6e12b8d9d27c5de2ba455e9405bb8d468b762d3d
rotlesnake <rotlesnake@yandex.ru>
2020-09-03 15:39:13 +0300

    latest

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,6 +1,10 @@
 <template>
-<v-layout>
-    <v-card>
+<section class="ma-2 text-center">
+
+	<login-logo />
+	<LoginLogotip />
+
+    <v-card max-width="500" class="ma-auto">
         <v-toolbar color="primary" dark flat>
             <v-toolbar-title>Вход в систему</v-toolbar-title>
             <v-spacer></v-spacer>
@@ -10,25 +14,29 @@
             <v-form @keyup.native.enter="doLogin()">
                 <v-text-field required outlined :disabled="isLoading" name="login" label="Фамилия Имя пользователя" v-model="auth.login" prepend-inner-icon="person" type="text"></v-text-field>
                 <v-text-field counter outlined :disabled="isLoading" name="password" label="Пароль" prepend-inner-icon="lock" v-model="auth.password" type="password"></v-text-field>
-                <v-alert dense outlined v-if="message.length > 0" type="error">{{
-            message
-          }}</v-alert>
+                <v-alert dense outlined v-if="message.length > 0" type="error">{{message}}</v-alert>
             </v-form>
         </v-card-text>
 
+		<v-divider />
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn type="submit" color="primary" @click="doLogin()" :loading="isLoading">
+            <v-btn type="submit" color="primary" @click="doLogin1()" :loading="isLoading">
                 <v-icon left>vpn_key</v-icon>Войти
             </v-btn>
         </v-card-actions>
     </v-card>
-</v-layout>
+
+</section>
 </template>
 
 <script>
 export default {
-    layout: "empty",
+	layout: "empty",
+	
+	components:{
+		"LoginLogotip":()=>import('@/components/auth/LoginLogo.vue')
+	},
 
     data() {
         return {
@@ -42,13 +50,12 @@ export default {
     },
 
     methods: {
-        doLogin() {
-            this.$store
-                .dispatch("auth/login", this.auth).then(result => {
-                    console.log("result", result);
-                }).catch(e => {
-                    this.message = e.message;
-                });
+        doLogin1() {
+            this.$store.dispatch("auth/login", this.auth).then(result => {
+                console.log("result", result);
+            }).catch(e => {
+                this.message = e.message;
+            });
         },
 
         async doLogin2() {
