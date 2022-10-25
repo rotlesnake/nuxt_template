8c24e0057bb95f3cd0438ed17618843f4aecacf5
Rotlesnake <rotlesnake@yandex.ru>
2022-10-25 15:24:53 +0300

    Update api.js

--- plugins/api.js
+++ plugins/api.js
@@ -1,13 +1,13 @@
 export default (context, inject) => {
 
-    inject('api', async (module, controller, method, args, params) => {
+    inject('api', async (module, controller, method, args, params, axiosSettings) => {
         try {
             controller = controller || 'index';
             method = method || 'index';
             if (args && params) method += "/"+args;
             if (args && !params) params = args;
 
-            return await context.$axios['$'+(params ? 'post' : 'get')]('/'+module+'/'+controller+'/'+method, params);
+            return await context.$axios['$'+(params ? 'post' : 'get')]('/'+module+'/'+controller+'/'+method, params, axiosSettings);
         } catch (e) {
             throw e;
         }
